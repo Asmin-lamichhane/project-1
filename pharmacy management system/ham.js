06eac9be3914ad49d090896a4f8d5ef9c626f9e0
Asmin-lamichhane <asminlamichhane02@gmail.com>
2025-03-01 22:49:29 +0545

    Delete pharmacy management system directory

--- pharmacy management system/ham.js
+++
@@ -1,6 +0,0 @@
-function toggleMenu() {
-    const sidenav = document.getElementById("sidenav");
-    sidenav.classList.toggle("active");
-    sidenav.classList.toggle("hidden");
-}
-
--- pharmacy management system/sup_delete.php
+++
@@ -1,8 +0,0 @@
-<?php
-	include "config.php";
-	$sql="DELETE FROM suppliers where sid='$_GET[id]'";
-	if ($conn->query($sql))
-	header("location:supplier-view.php");
-	else
-	echo "error";
-?>
